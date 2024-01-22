9166671a3a06b631243e6d35b91e6e4604ba0d2a
MalekJ19 <157360476+MalekJ19@users.noreply.github.com>
2024-01-22 11:23:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.Header('BreakFast Menu')
+streamlit.header('BreakFast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale,Spinach & Rocket Smoothie')
