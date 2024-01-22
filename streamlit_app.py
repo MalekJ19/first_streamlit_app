bc97515305e6e62766527eb824b05f71fd97a857
MalekJ19 <157360476+MalekJ19@users.noreply.github.com>
2024-01-22 11:20:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.Header('BreakFast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled free-Range Egg');
+streamlit.text('Hard-Boiled free-Range Egg')
 
