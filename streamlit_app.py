9aa8c6b9feef760038d90a21875e12569fced10c
MalekJ19 <157360476+MalekJ19@users.noreply.github.com>
2024-01-23 15:38:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,15 +45,7 @@ add_my_fruit=streamlit.text_input('What fruit would you like to add ?','Jackfrui
 streamlit.write('The user entered ', add_my_fruit)
 
 
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
