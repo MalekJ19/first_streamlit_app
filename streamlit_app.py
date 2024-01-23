0a960154de1fc4b22d2f87a89ec7c0d7db5fa5f4
MalekJ19 <157360476+MalekJ19@users.noreply.github.com>
2024-01-23 15:39:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,14 +37,8 @@ except URLError as e :
 streamlit.error()
 
 
-
-
-
-
 add_my_fruit=streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
-
-
 
 
 streamlit.stop()
